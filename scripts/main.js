0860183ad16b794cc00b5cf93f81bcf9988331f5
Achyut Dwivedi <cooldudeachyut@gmail.com>
2021-07-02 15:31:20 +0530

    Changes grid to 800x800

--- scripts/main.js
+++ scripts/main.js
@@ -4,9 +4,18 @@ let grid = document.createElement("div");
 grid.id = "grid-container";
 container.appendChild(grid);
 
-for (let i = 1; i <= 16; i++)
+let changeColor = (event) => {
+	event.target.setAttribute("style", "background-color:rgb(0, 0, 0);");
+}
+
+let changeColorBack = (event) => {
+	event.target.setAttribute("style", "background-color:rgb(255, 255, 255);");
+}
+
+for (let i = 1; i <= 6400; i++)
 {
 	let gridElem = document.createElement("div");
 	gridElem.classList.add("grid-element");
+	gridElem.addEventListener("mouseover", changeColor);
 	grid.appendChild(gridElem);
 }
